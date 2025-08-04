0fb1eaac9811b7fde5e35016db2f4b608fc1b93f
Eshwar2604 <hollaeshwar@gmail.com>
2025-08-04 23:32:32 +0530

    Add files via upload

---
+++ dht11.ino
@@ -0,0 +1,31 @@
+#include <DHT.h>
+
+#define DHTPIN 4        // Pin where DHT11 data is connected
+#define DHTTYPE DHT11   // DHT11 sensor type
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+}
+
+void loop() {
+  float h = dht.readHumidity();
+  float t = dht.readTemperature(); // Celsius
+
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT11!");
+    return;
+  }
+
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.println(" °C");
+
+  delay(2000); // Wait 2 seconds between readings
+}
